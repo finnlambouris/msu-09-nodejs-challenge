edcfb2d66fe258e083758e87be96ab1f35ccf257
finntendoverse <finnlambouris616@gmail.com>
2024-04-02 17:02:11 -0400

    added code comments

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const inquirer = require('inquirer');
 const fileSystem = require('fs');
 
 // function that handles all the prompts and user input
-let initializePrompting = function() {
-    // Title
-    let getProjectTitle = async function() {
-        let projectTitle = await inquirer.prompt(
+let initializePrompting = function() {                          // WHEN the initializePrompting function is called
+    // Title prompt
+    let getProjectTitle = async function() {                                                                                                                                        // WHEN the getProjectTitle function is called
+        let projectTitle = await inquirer.prompt(                                                                                                                                   // THEN the projectTitle will be saved when entered
             [
                 {
                     name: 'title',
@@ -16,18 +16,18 @@ let initializePrompting = function() {
             ]
         )
 
-        if (projectTitle.title === '') {
-            projectTitle.title = 'Untitled project';
+        if (projectTitle.title === '') {                                                                                                                                            // IF the project title is left empty
+            projectTitle.title = 'Untitled project';                                                                                                                                // THEN it will be titled "untitled project"
         }
 
-        getProjectDescription(projectTitle);
+        getProjectDescription(projectTitle);                                                                                                                                        // THEN the user is prompted to enter their project description
     }
 
-    getProjectTitle();
+    getProjectTitle();                                          // THEN the user is prompted to enter their project title
 
-    // Description
-    let getProjectDescription = async function(projectTitle) {
-        let projectDescription = await inquirer.prompt(
+    // Description prompt
+    let getProjectDescription = async function(projectTitle) {                                                                                                                      // WHEN the getProjectDescription function is called
+        let projectDescription = await inquirer.prompt(                                                                                                                             // THEN the projectDescription will be saved when entered
             [
                 {
                     name: 'description',
@@ -37,16 +37,16 @@ let initializePrompting = function() {
             ]
         )
 
-        if (projectDescription.description === '') {
-            projectDescription.description = 'N/A';
+        if (projectDescription.description === '') {                                                                                                                                // IF the project description is left empty
+            projectDescription.description = 'N/A';                                                                                                                                 // THEN the description will be set to "N/A"
         }
 
-        getInstallationInstructions(projectTitle, projectDescription);
+        getInstallationInstructions(projectTitle, projectDescription);                                                                                                              // THEN the user is prompted to enter their installation instructions
     }
 
-    // Installation Instructions
-    let getInstallationInstructions = async function(projectTitle, projectDescription) {
-        let installationInstructions = await inquirer.prompt(
+    // Installation Instructions prompt
+    let getInstallationInstructions = async function(projectTitle, projectDescription) {                                                                                            // WHEN the getInstallationInstructions function is called
+        let installationInstructions = await inquirer.prompt(                                                                                                                       // THEN the installationInstructions will be saved when entered
             [
                 {
                     name: 'installation',
@@ -56,16 +56,16 @@ let initializePrompting = function() {
             ]
         )
 
-        if (installationInstructions.installation === '') {
-            installationInstructions.installation = 'N/A';
+        if (installationInstructions.installation === '') {                                                                                                                         // IF the installation instructions are left empty
+            installationInstructions.installation = 'N/A';                                                                                                                          // THEN the installation instructions will be set to "N/A"
         }
 
-        getUsageInformation(projectTitle, projectDescription, installationInstructions);
+        getUsageInformation(projectTitle, projectDescription, installationInstructions);                                                                                            // THEN the user is prompted to enter their usage information
     }
 
-    // Usage
-    let getUsageInformation = async function(projectTitle, projectDescription, installationInstructions) {
-        let usageInformation = await inquirer.prompt(
+    // Usage prompt
+    let getUsageInformation = async function(projectTitle, projectDescription, installationInstructions) {                                                                          // WHEN the getUsageInformation function is called
+        let usageInformation = await inquirer.prompt(                                                                                                                               // THEN the usageInformation will be saved when entered
             [
                 {
                     name: 'usage',
@@ -75,16 +75,16 @@ let initializePrompting = function() {
             ]
         )
 
-        if (usageInformation.usage === '') {
-            usageInformation.usage = 'N/A';
+        if (usageInformation.usage === '') {                                                                                                                                        // IF the usage information is left empty
+            usageInformation.usage = 'N/A';                                                                                                                                         // THEN the usage information will be set to "N/A"
         }
 
-        getLicense(projectTitle, projectDescription, installationInstructions, usageInformation);
+        getLicense(projectTitle, projectDescription, installationInstructions, usageInformation);                                                                                   // THEN the user is prompted to enter their license
     }
 
-    // License
-    let getLicense = async function(projectTitle, projectDescription, installationInstructions, usageInformation) {
-        let license = await inquirer.prompt(
+    // License prompt
+    let getLicense = async function(projectTitle, projectDescription, installationInstructions, usageInformation) {                                                                 // WHEN the getLicense function is called
+        let license = await inquirer.prompt(                                                                                                                                        // THEN the license will be saved when entered
             [
                 {
                     name: 'license',
@@ -94,24 +94,24 @@ let initializePrompting = function() {
                 }
             ]
         )
-        if (license.license === 'MIT License') {
-            license.badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-        } else if (license.license === 'Apache License') {
-            license.badge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
-        } else if (license.license === 'GPLv3 License') {
-            license.badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
-        } else if (license.license === 'Mozilla Public License 2.0') {
-            license.badge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
-        } else {
-            license.badge = '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+        if (license.license === 'MIT License') {                                                                                                                                    // IF the MIT License is selected
+            license.badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';                                          // THEN a badge property will be created for it
+        } else if (license.license === 'Apache License') {                                                                                                                          // IF the Apache License is selected
+            license.badge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';                                   // THEN a badge property will be created for it
+        } else if (license.license === 'GPLv3 License') {                                                                                                                           // IF the GPLv3 License is selected
+            license.badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';                                      // THEN a badge property will be created for it
+        } else if (license.license === 'Mozilla Public License 2.0') {                                                                                                              // IF the Mozilla Public License 2.0 is selected
+            license.badge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'                          // THEN a badge property will be created for it
+        } else {                                                                                                                                                                    // IF the BSD 3-Clause License is selected
+            license.badge = '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'                               // THEN a badge property will be created for it
         }
 
-        getCredits(projectTitle, projectDescription, installationInstructions, usageInformation, license);
+        getCredits(projectTitle, projectDescription, installationInstructions, usageInformation, license);                                                                          // THEN the user is prompted to enter their credits
     }
 
-    // Credits
-    let getCredits = async function(projectTitle, projectDescription, installationInstructions, usageInformation, license) {
-        let credits = await inquirer.prompt(
+    // Credits prompt
+    let getCredits = async function(projectTitle, projectDescription, installationInstructions, usageInformation, license) {                                                        // WHEN the getCredits function is called
+        let credits = await inquirer.prompt(                                                                                                                                        // THEN the credits will be saved when entered
             [
                 {
                     name: 'credits',
@@ -121,16 +121,16 @@ let initializePrompting = function() {
             ]
         )
 
-        if (credits.credits === '') {
-            credits.credits = 'N/A';
+        if (credits.credits === '') {                                                                                                                                               // IF the credits are left empty
+            credits.credits = 'N/A';                                                                                                                                                // THEN the credits will be set to "N/A"
         }
 
-        getTestInstructions(projectTitle, projectDescription, installationInstructions, usageInformation, license, credits);
+        getTestInstructions(projectTitle, projectDescription, installationInstructions, usageInformation, license, credits);                                                        // THEN the user is prompted to enter their test instructions
     }
 
-    // Test Instructions
-    let getTestInstructions = async function(projectTitle, projectDescription, installationInstructions, usageInformation, license, credits) {
-        let testInstructions = await inquirer.prompt(
+    // Test Instructions prompt
+    let getTestInstructions = async function(projectTitle, projectDescription, installationInstructions, usageInformation, license, credits) {                                      // WHEN the getTestInstructions function is called
+        let testInstructions = await inquirer.prompt(                                                                                                                               // THEN the test instructions will be saved when entered
             [
                 {
                     name: 'testing',
@@ -140,16 +140,16 @@ let initializePrompting = function() {
             ]
         )
 
-        if (testInstructions.testing === '') {
-            testInstructions.testing = 'N/A';
+        if (testInstructions.testing === '') {                                                                                                                                      // IF the test instructions are left empty
+            testInstructions.testing = 'N/A';                                                                                                                                       // THEN the test instructions will be set to "N/A"
         }
 
-        getGitHubUsername(projectTitle, projectDescription, installationInstructions, usageInformation, license, credits, testInstructions);
+        getGitHubUsername(projectTitle, projectDescription, installationInstructions, usageInformation, license, credits, testInstructions);                                        // THEN the user is prompted to enter their GitHub Username
     }
 
-    // Questions
-    let getGitHubUsername = async function(projectTitle, projectDescription, installationInstructions, usageInformation, license, credits, testInstructions) {
-        let gitHubUsername = await inquirer.prompt(
+    // GitHub username prompt
+    let getGitHubUsername = async function(projectTitle, projectDescription, installationInstructions, usageInformation, license, credits, testInstructions) {                      // WHEN the getGitHubUsername function is called
+        let gitHubUsername = await inquirer.prompt(                                                                                                                                 // THEN the username will be saved when entered
             [
                 {
                     name: 'GitHub',
@@ -159,11 +159,12 @@ let initializePrompting = function() {
             ]
         )
 
-        getEmailAddress(projectTitle, projectDescription, installationInstructions, usageInformation, license, credits, testInstructions, gitHubUsername);
+        getEmailAddress(projectTitle, projectDescription, installationInstructions, usageInformation, license, credits, testInstructions, gitHubUsername);                          // THEN the user is prompted to enter their email address
     }
 
-    let getEmailAddress = async function(projectTitle, projectDescription, installationInstructions, usageInformation, license, credits, testInstructions, gitHubUsername) {
-        let email = await inquirer.prompt(
+    // Email address prompt
+    let getEmailAddress = async function(projectTitle, projectDescription, installationInstructions, usageInformation, license, credits, testInstructions, gitHubUsername) {        // WHEN the getEmailAddress function is called
+        let email = await inquirer.prompt(                                                                                                                                          // THEN the email address will be saved when entered
             [
                 {
                     name: 'email',
@@ -173,12 +174,12 @@ let initializePrompting = function() {
             ]
         )
 
-        generateREADME(projectTitle, projectDescription, installationInstructions, usageInformation, license, credits, testInstructions, gitHubUsername, email);
+        generateREADME(projectTitle, projectDescription, installationInstructions, usageInformation, license, credits, testInstructions, gitHubUsername, email);                    // THEN a README will be generated with all of the information inputted by the user
     }
 }
 
 // function to generate the README
-let generateREADME = function (projectTitle, projectDescription, installationInstructions, usageInformation, license, credits, testInstructions, gitHubUsername, email) {
+let generateREADME = function (projectTitle, projectDescription, installationInstructions, usageInformation, license, credits, testInstructions, gitHubUsername, email) {           // WHEN the generateREADME function is called
 let READMEcontent = `
 # ${projectTitle.title} ${license.badge}
 
@@ -213,9 +214,9 @@ ${testInstructions.testing}
 Here's how you can get in contact with me to ask any questions!
 * [Visit my GitHub](https://github.com/${gitHubUsername.GitHub})
 * [Email me](mailto:${email.email})
-`
-fileSystem.writeFile('./generatedREADMEs/README.md', READMEcontent, (err) => err ? console.error("there was an error") : console.log("done!"));
+`                                                                                                                                                                                   // THEN the content within the README is set to everything entered by the user
+fileSystem.writeFile('./generatedREADMEs/README.md', READMEcontent, (err) => err ? console.error("your README was not generated") : console.log("your README was generated!"));     // THEN the README file is created
 }
 
-// function to initialize the prompts
+// function to initialize the prompts when the file is run
 initializePrompting();
