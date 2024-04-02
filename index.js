a20cb26d7c105cb55459c0dc7961ab081187a89a
finntendoverse <finnlambouris616@gmail.com>
2024-04-01 22:06:18 -0400

    installed inquirer and wrote outline of prompts

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Title
+"What is the title of your project?"
+
+// Table of Contents
+// Description, Installation Instructions, Usage, License, Credits, Test Instructions, and Questions
+
+// Description
+"Enter your project's description."
+
+// Installation Instructions
+"Enter any installation instructions for your project."
+
+// Usage
+"Enter your project's usage information."
+
+// License
+// select license
+
+// Credits
+"Enter any credits for outside sources that helped you create your project."
+
+// Test Instructions
+"Enter any testing instructions for your project."
+
+// Questions
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
